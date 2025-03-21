ba60cb8977cd3429f693196b38d7ba57a7e3cb70
Kaju2002 <IT22224002@my.sliit.lk>
2025-03-21 12:07:33 +0530

    complete order

--- server/route/orderRoute.js
+++ server/route/orderRoute.js
@@ -1,6 +1,6 @@
 // Routes: orderRoutes.js
 import express from "express";
-import { createOrder, deleteOrder, getAllOrders, updateOrder } from "../controller/orderController.js";
+import { completeOrder, createOrder, deleteOrder, getAllOrders, takeOrder, updateOrder } from "../controller/orderController.js";
 
 
 const orderRouter = express.Router();
@@ -10,5 +10,9 @@ orderRouter.get("/get-all-orders", getAllOrders);
 orderRouter.get("/:id", getAllOrders);
 orderRouter.put("/update/:id", updateOrder);
 orderRouter.delete("/delete/:id", deleteOrder);
+orderRouter.post("/take-order", takeOrder);
+orderRouter.put("/completeOrder", completeOrder);
+
+
 
 export default orderRouter;
