56011e5ad9f2c522548fd3709ea4740d3b8a01f6
Kaju2002 <IT22224002@my.sliit.lk>
2025-03-21 16:07:46 +0530

    small

--- deliveryDashBoard/src/App.js
+++ deliveryDashBoard/src/App.js
@@ -6,11 +6,15 @@ import Dashboard from './components/Dashboard';
 import Home from './pages/Home';
 import Users from './pages/Users';
 import Settings from './pages/Settings';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <Router>
       <div style={{ display: 'flex' }}>
+      <ToastContainer/>
+
         <Sidebar />
         <div style={{ flex: 1 }}>
           <Navbar />
